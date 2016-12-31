67063f804c6a05621db44f90a8e3737f304caa1a
AROLING <guilin_luos@hotmail.com>
2016-12-31 19:08:24 +0800

    初始化提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,24 +3,27 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello frank',
-    userInfo: {}
+    background: ['green', 'red', 'yellow'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 1200,    
+    
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
   onLoad: function () {
-    console.log('onLoad frankxieke')
-    var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    /*app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
         userInfo:userInfo
       })
-    })
+    })*/
   }
 })
